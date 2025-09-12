bf9e64cc272193c26f0396fab28effbf27be0b17
aig2088-gummi <aig2088@nyu.edu>
2025-09-12 14:13:12 -0400

    Add files via upload

--- gettingStarted.py
+++ gettingStarted.py
@@ -27,10 +27,28 @@ def welcome_assignment_answers(question):
 
 
 if __name__ == "__main__":
-
-
+    print(welcome_assignment_answers(
+        "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?"))
     print(welcome_assignment_answers("Are encoding and encryption the same? - Yes/No"))
-    print(welcome_assignment_answers("What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number"))
+    print(welcome_assignment_answers("Is it possible to decrypt a message without a key? - Yes/No"))
+    print(welcome_assignment_answers("Is it possible to decode a message without a key? - Yes/No"))
+    print(welcome_assignment_answers("Is a hashed message supposed to be un-hashed? - Yes/No"))
+    print(welcome_assignment_answers(
+        "What is the SHA256 hashing value of your NYU email and use the answer in your code - "))
     print(welcome_assignment_answers("Is MD5 a secured hashing algorithm? - Yes/No"))
-
+    print(welcome_assignment_answers(
+        "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number"))
+    print(welcome_assignment_answers(
+        "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number"))
+    
+#Questions:
+#"In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
+#"Are encoding and encryption the same? - Yes/No":
+#"Is it possible to decrypt a message without a key? - Yes/No":
+#"Is it possible to decode a message without a key? - Yes/No":
+#"Is a hashed message supposed to be un-hashed? - Yes/No":
+#"What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
+#"Is MD5 a secured hashing algorithm? - Yes/No":
+#"What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
+#"What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
 
